3dd28877929f0b5182918b2067212e5e02ff5a3c
aladdin4u <taofikakinkumi@gmail.com>
2023-01-08 17:13:59 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors")
 const PORT = 5000;
 
 dotenv.config();
-
+app.use(cors());
 const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = "https://fqpkdxmgtkfhuuqylnbv.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-app.use("/", async (req, res) => {
+app.get("/", async (req, res) => {
   let { data: todo_sample, error } = await supabase
     .from("todo_sample")
     .select("*");
   console.log(todo_sample);
 
   res.send("<h2>My first superbase project</h2>");
+});
+
+app.post("/create", async (req, res) => {
+  const { data, error } = await supabase
+  .from('todo_sample')
+  .insert([
+    { 
+      task_title: 'someValue', 
+      task_description: 'otherValue',
+      completed: false
+    },
+  ])
+});
+
+app.put("/update", async (req, res) => { 
+  const { data, error } = await supabase
+  .from('todo_sample')
+  .update({ other_column: 'otherValue' })
+  .eq('some_column', 'someValue')
+});
+
+app.delete("/delete", async (req, res) => {
+  const { data, error } = await supabase
+  .from('todo_sample')
+  .delete()
+  .eq('some_column', 'someValue')
 });
 
 app.listen(PORT, () => console.log(`starting server on ${PORT}`));
